0a9ff218f5ba934cf5aef0f5f1200983c24465a2
eric <eric.connelly@essencedigital.com>
2014-03-11 20:33:09 -0400

    Fixing the font size on welcome index.html page

--- js/main.js
+++ js/main.js
@@ -5,5 +5,20 @@ $('#sidebar').affix({
 });
 
 $(document).ready(function() {
-    console.log('js ready to act');
+    var resizeTitle = function() {
+        var width = parseInt($(".title__container").width());
+        $(".title.main").css(
+            'font-size',
+            Math.min(Math.max(width/3, 30), 78)
+        );
+        $(".title.date-title").css(
+            'font-size',
+            Math.min(Math.max(width/4.5, 16), 48)
+        );
+        $('.title').css('display', 'block');
+    };
+
+    window.onresize = resizeTitle;
+
+    resizeTitle();
 });
